0c0c0efa617fb9f78a65976a6347faa40c087163
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2018-10-15 21:36:39 +0500

    Update PagesToFolders.sketchplugin/Contents/Sketch/export.js

--- PagesToFolders.sketchplugin/Contents/Sketch/export.js
+++ PagesToFolders.sketchplugin/Contents/Sketch/export.js
@@ -7,17 +7,16 @@ function exportAllPages(context) {
 	var pages = doc.pages();
 	var fileFolder = doc.fileURL().path().split(doc.displayName())[0];
 
-	var rootFolder = fileFolder + "Pages to Folders/";
+	var filename = context.document.fileURL().lastPathComponent().split('.')[0].replace(/ \([\s\S]*?\)/g, '');
+	var rootFolder = "/Users/pavellaptev/Downloads/" + filename + "/";
 
 	Utilities.deleteAndCreateFolder(rootFolder);
-
-	var pageCounter = 1;
 
 	for (var i = 0; i < pages.count(); i++) {
 		var currentPage = pages[i];
 
 		if (pages[i].name() != "Symbols") {
-			var pageFolderPath = rootFolder + pageCounter.toString() + " \u2013 " + pages[i].name() + "/";
+			var pageFolderPath = rootFolder + pages[i].name() + "/";
 			Utilities.createFolder(pageFolderPath);
 
 			doc.setCurrentPage(currentPage);
@@ -25,18 +24,19 @@ function exportAllPages(context) {
 			var artboards = doc.currentPage().artboards();
 
 			for (var j = 0; j < artboards.count(); j++) {
-				var artboard = artboards[j];
+                var artboard = artboards[j];
+                var request = MSExportRequest.exportRequestsFromExportableLayer_inRect_useIDForName(artboard, artboard.absoluteRect().rect(), false)[0]
+                request.scale = 2
+
 				var artboardNameWithDashesInsteadOfSlashes = Utilities.cleanArtboardName(String(artboard.name()));
 
 				// TODO Try and get the exportable options for each artboard,
 				// and export it that way. If there are no export options,
 				// export the file as a @1x .png file.
-				doc.saveArtboardOrSlice_toFile_(artboard, pageFolderPath + artboardNameWithDashesInsteadOfSlashes + ".png");
+				doc.saveArtboardOrSlice_toFile(request, pageFolderPath + artboardNameWithDashesInsteadOfSlashes.replace(/ /g,"-") + ".png");
 			}
-
-			pageCounter = pageCounter + 1;
 		}
 	}
 
-	application.alert("You can find the exported files in a folder next to your original file.", "Export Successful!");
+	application.alert("Done!", "Your files is ready")
 };
