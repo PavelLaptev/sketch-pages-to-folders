b8e999cf3e132e1a0fad8004f457a89dbf77b277
Pavel Laptev <pavellaptev@Pavels-MacBook-Pro.local>
2018-11-28 03:48:47 +0500

    added folder select option

--- PagesToFolders.sketchplugin/Contents/Sketch/export.js
+++ PagesToFolders.sketchplugin/Contents/Sketch/export.js
@@ -5,38 +5,39 @@ function exportAllPages(context) {
 	var application = sketch.Application();
 	var doc = context.document;
 	var pages = doc.pages();
-	var fileFolder = doc.fileURL().path().split(doc.displayName())[0];
 
-	var filename = context.document.fileURL().lastPathComponent().split('.')[0].replace(/ \([\s\S]*?\)/g, '');
-	var rootFolder = "/Users/pavellaptev/Downloads/" + filename + "/";
+	var filename = context.document.fileURL().lastPathComponent().split('.')[0].replace(/ \([\s\S]*?\)/g, '').replace(/ /g,"_").toLowerCase();
+	var file_path = Utilities.selectFolder();
+	var rootFolder = file_path + filename + "/";
 
 	Utilities.deleteAndCreateFolder(rootFolder);
 
-	for (var i = 0; i < pages.count(); i++) {
-		var currentPage = pages[i];
+	if (file_path) {
+		for (var i = 0; i < pages.count(); i++) {
+			var currentPage = pages[i];
 
-		if (pages[i].name() != "Symbols") {
-			var pageFolderPath = rootFolder + pages[i].name() + "/";
-			Utilities.createFolder(pageFolderPath);
+			if (pages[i].name() != "Symbols") {
+				var pageFolderPath = rootFolder + pages[i].name().replace(/ /g,"_").toLowerCase() + "/";
+				Utilities.createFolder(pageFolderPath);
 
-			doc.setCurrentPage(currentPage);
+				doc.setCurrentPage(currentPage);
 
-			var artboards = doc.currentPage().artboards();
+				var artboards = doc.currentPage().artboards();
 
-			for (var j = 0; j < artboards.count(); j++) {
-                var artboard = artboards[j];
-                var request = MSExportRequest.exportRequestsFromExportableLayer_inRect_useIDForName(artboard, artboard.absoluteRect().rect(), false)[0]
-                request.scale = 2
+				for (var j = 0; j < artboards.count(); j++) {
+					var artboard = artboards[j];
+					var request = MSExportRequest.exportRequestsFromExportableLayer_inRect_useIDForName(artboard, artboard.absoluteRect().rect(), false)[0]
+					request.scale = 2
 
-				var artboardNameWithDashesInsteadOfSlashes = Utilities.cleanArtboardName(String(artboard.name()));
+					var artboardNameWithDashesInsteadOfSlashes = Utilities.cleanArtboardName(String(artboard.name()).toLowerCase());
 
-				// TODO Try and get the exportable options for each artboard,
-				// and export it that way. If there are no export options,
-				// export the file as a @1x .png file.
-				doc.saveArtboardOrSlice_toFile(request, pageFolderPath + artboardNameWithDashesInsteadOfSlashes.replace(/ /g,"-") + ".png");
+					// TODO Try and get the exportable options for each artboard,
+					// and export it that way. If there are no export options,
+					// export the file as a @1x .png file.
+					doc.saveArtboardOrSlice_toFile(request, pageFolderPath + artboardNameWithDashesInsteadOfSlashes.replace(/ /g,"_") + ".png");
+				}
 			}
 		}
+		application.alert("Done!", "Your files are ready")
 	}
-
-	application.alert("Done!", "Your files is ready")
 };
--- PagesToFolders.sketchplugin/Contents/Sketch/utilities.js
+++ PagesToFolders.sketchplugin/Contents/Sketch/utilities.js
@@ -1,33 +1,58 @@
-var Utilities = { };
+var Utilities = {};
 
-Utilities.createFolder = function (path) {
-	var fileManager = NSFileManager.defaultManager();
-
-	// TODO Check if this failes to create the directory,
-	// break the flow and alert the user.
-	fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error_(path, true, nil, nil);
-};
-
-Utilities.deleteFolder = function (path) {
-	var fileManager = NSFileManager.defaultManager();
-
-	fileManager.removeItemAtPath_error(path, nil);
-};
-
-Utilities.deleteAndCreateFolder = function (path) {
-	Utilities.deleteFolder(path);
-	Utilities.createFolder(path);
-};
-
-Utilities.cleanArtboardName = function (name) {
-	var cleanedUpName = "";
-
-	for (var i = 0; i < name.length; i++) {
-		if (name[i] != "/")
-			cleanedUpName += name[i];
-		else
-			cleanedUpName += "-";
+Utilities.selectFolder = function (){
+	//open a window to select a folder to save to
+	var panel = [NSOpenPanel openPanel];
+	[panel setCanChooseDirectories:true];
+	[panel setCanCreateDirectories:true];
+  
+	//checks if user clicks open in window
+	var clicked = [panel runModal];
+	if (clicked == NSFileHandlingPanelOKButton) {
+  
+	  var isDirectory = true;
+	  var firstURL = [[panel URLs] objectAtIndex:0];
+	  var unformattedURL = [NSString stringWithFormat:@"%@", firstURL];
+  
+	  //makes sure spaces aren't formatted to %20
+	  var file_path = [unformattedURL stringByRemovingPercentEncoding];
+  
+	  //removes file:// from path
+	  if (0 === file_path.indexOf("file://")) {
+		 file_path = file_path.substring(7);
+		 return file_path;
+	  }
 	}
+  }
 
-	return cleanedUpName;
+Utilities.createFolder = function(path) {
+    var fileManager = NSFileManager.defaultManager();
+    fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error_(
+        path,
+        true,
+        nil,
+        nil
+    );
+};
+
+Utilities.deleteFolder = function(path) {
+    var fileManager = NSFileManager.defaultManager();
+
+    fileManager.removeItemAtPath_error(path, nil);
+};
+
+Utilities.deleteAndCreateFolder = function(path) {
+    Utilities.deleteFolder(path);
+    Utilities.createFolder(path);
+};
+
+Utilities.cleanArtboardName = function(name) {
+    var cleanedUpName = "";
+
+    for (var i = 0; i < name.length; i++) {
+        if (name[i] != "/") cleanedUpName += name[i];
+        else cleanedUpName += "-";
+    }
+
+    return cleanedUpName;
 };
